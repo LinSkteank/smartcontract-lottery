b8cad49c8fcb145e4d158d2187577017f96e1796
LinSkteank <80599444+LinSkteank@users.noreply.github.com>
2021-09-22 06:55:07 +0300

    yaml config + import Agregator

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,38 @@
+// SPDX-Licence-Identifier: MIT
 pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
 
-    function enter() public {}
-    function getEntranceFee() public {}
+    address payable[] public players;
+    uint public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10 ** 18);
+        ethUsdPriceFeed = AggregatorV3Inteface();
+
+    }
+
+
+
+    function enter() public payable{
+
+        players.push(msg.sender);
+    }
+
+
+    function getEntranceFee() public view returns(uint256) {}
+
+
     function startLottery() public {}
+
+
     function endLottery() public {}
+
+
+    
 }
+
